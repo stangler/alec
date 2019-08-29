511ea3a4692619c73e521f75f7be815de74f77a5
stangler <temp@temp.com>
2019-08-29 15:26:47 +0900

    Modify led01.py and led02.py

--- led02.py
+++ led02.py
@@ -5,12 +5,9 @@ import RPi.GPIO as GPIO # GPIO を利用する
 import time             # sleep を利用する
 
 PNO = 4   # 対象の GPIO ポート番号
-#PNO = 7  # 対象の GPIO ピン番号
-
 
 # GPIO の初期化
 GPIO.setmode(GPIO.BCM)    # BCM モード（ポート番号で指定する方法）
-#GPIO.setmode(GPIO.BOARD) # BCM モード（ピン番号で指定する方法）
 GPIO.setup(PNO, GPIO.OUT) # 指定した番号を out に設定
 
 # 10 回繰り返す
